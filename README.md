3ac15e5cd3e7c055c8e368aa0f6e3ab404611e5c
Alkesst <alec.augal@gmail.com>
2017-10-16 21:12:32 +0200

    Finished bot :)

---
+++ README.md
@@ -0,0 +1,13 @@
+# @ZXMAST3R Bot
+Just for fun, does nothing
+
+#### Twitter users:
+Original idea:
+[@ZXMAST3R][1]
+
+Creator:
+[@Alkesstt][2]
+
+
+  [1]: http://twitter.com/ZXMAST3R
+  [2]: http://twitter.com/Alkesstt
---
+++ boi.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import json
+import time
+import random
+import tweepy
+
+
+def main():
+    json_config = open("tokens.json", 'r')
+    tokens = json.load(json_config)
+    json_config.close()
+    consumer_key = tokens["consumer_key"]
+    consumer_secret = tokens["consumer_secret"]
+    access_token = tokens["access_token"]
+    access_token_secret = tokens["access_token_secret"]
+
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+    api = tweepy.API(auth, wait_on_rate_limit=True)
+    cont = 0
+    while True:
+        lista_tweets = read_from_file()
+        api.update_status(lista_tweets[cont % len(lista_tweets)])
+        time.sleep(900)
+        tweet = random_ZXMAST3R_tweet(api)
+        api.update_status(tweet)
+        time.sleep(900)
+        cont += 10
+
+
+def read_from_file():
+    opened_file = open('gilipollas_bot.txt', 'rb')
+    lista = []
+    has_next = True
+    while has_next:
+        linea = opened_file.readline()
+        if not linea:
+            has_next = False
+        else:
+            lista.append(linea)
+    return lista
+
+
+def random_ZXMAST3R_tweet(api):
+    json_config = open("tokens.json", 'r')
+    tokens = json.load(json_config)
+    json_config.close()
+    lista = api.user_timeline(tokens["user_id"])
+    tweet: str = ''
+    contador = 0
+    while len(tweet) < 50 and contador < len(lista):
+        splitted = lista[contador].text.split()
+        lines = len(splitted)
+        random_pos = int(round(random.random() * lines, 0)) - 4
+        cont = 0
+        palabras = 0
+        while palabras < 3 and cont < 20:
+            word = splitted[(random_pos + cont) % lines]
+            if 'http' not in word and 'RT' not in word and '@' not in word:
+                tweet += ' ' + word
+                palabras += 1
+            cont += 1
+        contador += 1
+
+
+if __name__ == "__main__":
+    main()
